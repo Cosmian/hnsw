3662db8eb0f03c4275dbfaf02de329671cc32244
Adam Khayam <adamkhayam@Adams-MacBook-Pro.local>
2024-07-01 14:19:02 +0200

    initial code

---
+++ rust/hnsw/src/main.rs
@@ -0,0 +1,37 @@
+mod hnsw;
+mod node;
+mod vector;
+
+use hnsw::HnswIndex;
+use rand::Rng;
+use vector::{EuclideanDistance, VectorItem};
+
+fn main() {
+    let hnsw = HnswIndex::new(10000, 1.0 / 3.0, 16, Box::new(EuclideanDistance));
+
+    // Add a larger number of items
+    for i in 0..1000 {
+        let mut rng = rand::thread_rng();
+        let vector: Vec<f64> = (0..10).map(|_| rng.gen_range(0.0..10.0)).collect();
+        let item = VectorItem { id: i, vector };
+        hnsw.add(item).unwrap();
+    }
+
+    let test_queries = vec![vec![1.0, 2.0], vec![5.0, 5.0], vec![9.0, 0.0]];
+
+    for query_vec in test_queries {
+        let query = VectorItem {
+            id: 9999,
+            vector: query_vec,
+        };
+        match hnsw.search(&query, 5) {
+            Ok(results) => {
+                println!("Search results for query {:?}:", query.vector);
+                for result in results {
+                    println!("Found item with id: {}", result.id);
+                }
+            }
+            Err(e) => println!("Search error: {}", e),
+        }
+    }
+}
